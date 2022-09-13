750d79a9859227c371634f23a1cbbefa4028ece4
mg62755 <mg62755@tamu.edu>
2022-09-13 18:07:53 -0500

    added working toggle button, favicon

---
+++ functions.js
@@ -0,0 +1,22 @@
+function toggle() {
+    var styleSheet = document.getElementById("style").href;
+    console.log(style.href);
+    console.log("Howdy");
+    if (styleSheet.match("coding.css")) {
+        console.log("coding");
+        document.getElementById("style").href = "spin.css";
+        localStorage.setItem("style", "spin.css");
+    } else {
+        console.log("spin");
+        document.getElementById("style").href = "coding.css";
+        localStorage.setItem("style", "coding.css");
+    }
+}
+
+window.onload = function() {
+    if (localStorage.getItem("style") === null) {
+        document.getElementById("style").href = "coding.css";
+    } else {
+        document.getElementById("style").href = localStorage.getItem("style");
+    }
+}
