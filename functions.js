ab21990744d86349bc0225abfcf4ba9e1b097aba
mg62755 <mg62755@tamu.edu>
2022-09-13 20:26:35 -0500

    started second css style sheet

--- functions.js
+++ functions.js
@@ -1,22 +1,21 @@
 function toggle() {
     var styleSheet = document.getElementById("style").href;
-    console.log(style.href);
-    console.log("Howdy");
+    // console.log(style.href);
+    // console.log("Howdy");
     if (styleSheet.match("coding.css")) {
-        console.log("coding");
+        // console.log("coding");
         document.getElementById("style").href = "spin.css";
         localStorage.setItem("style", "spin.css");
     } else {
-        console.log("spin");
+        // console.log("spin");
         document.getElementById("style").href = "coding.css";
         localStorage.setItem("style", "coding.css");
     }
 }
 
 window.onload = function() {
-    if (localStorage.getItem("style") === null) {
-        document.getElementById("style").href = "coding.css";
-    } else {
-        document.getElementById("style").href = localStorage.getItem("style");
+    if (!localStorage.getItem("style")) {
+        localStorage.setItem("style", "coding.css");
     }
+    document.getElementById("style").href = localStorage.getItem("style");
 }
